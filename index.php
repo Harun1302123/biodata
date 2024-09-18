74f4cbcef3289500b0aed02059690e12b1176980
HARUNUR RASHID <harunur@ba-systems.com>
2024-09-19 03:17:22 +0600

    some changes

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata</title>
+</head>
+<body>
+    <ul>
+        <li><a href="accordion.html">Accordion</a></li>
+        <li><a href="preview.html">Preview</a></li>
+        <li><a href="biodata.html">Biodata</a></li>
+        <li><a href="multi-step.html">Multi Step</a></li>
+        <li><a href="jsonForm.html">jsonForm</a></li>
+        <li><a href="jsonHtml.html">jsonHtml</a></li>
+    </ul>
+</body>
+</html>
